5a416f4c6815a873ac3798b45c67041cc2def408
SmartViking <smartestviking@gmail.com>
2011-04-10 07:42:29 -0700

    Edited Crazy_China_Pong.py via GitHub

--- Crazy_China_Pong.py
+++ Crazy_China_Pong.py
@@ -17,7 +17,7 @@
 """
 import sys, pygame
 from pygame.locals import *
-pygame.init() and pygame.display.set_caption('Crazy China Pong - 1.0 Beta 16')
+pygame.init() and pygame.display.set_caption('Crazy China Pong - 1.0 Beta 17')
 
 def main():
     score = 0
@@ -122,4 +122,5 @@ def main():
         #Update the new images or whatever
         pygame.display.update()
         clock.tick(90)
+
 if __name__ == "__main__": main()
